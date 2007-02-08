420dd65709388c48b26353227a57bb3de1fc02a7
Joaquim Rocha <jjmrocha@users.noreply.github.com>
2007-02-08 14:37:16 +0000

    *** empty log message ***

--- SRC/net/java/sjtools/security/Base64Util.java
+++ SRC/net/java/sjtools/security/Base64Util.java
@@ -19,140 +19,151 @@
  */
 package net.java.sjtools.security;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.Writer;
+import java.io.UnsupportedEncodingException;
 
 public class Base64Util {
-	public static final char[] MASTER_TABLE = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-			'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-			'+', '/' };
+	private static final int MAX_LINE_LENGTH = 76;
+	private static final byte NEW_LINE = (byte) '\n';
+	private static final byte EQUALS_SIGN = (byte) '=';
 
-	private static int DECODE_TABLE[] = new int[128];
-	private static final char PAD = '=';
-	private static final String CRLF = "\r\n";
-	
-	public static String encode(byte[] source) throws IOException {
-		StringWriter writer = new StringWriter();
-		
-		encode(new InputStreamReader(new ByteArrayInputStream(source)), writer);
-		
-		return writer.toString();
-	}
-	
-	public static byte[] decode(String source) throws IOException {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		OutputStreamWriter os = new OutputStreamWriter(out);
-		
-		decode(new StringReader(source), os);
-		
-		os.flush();
-		
-		return out.toByteArray();
-	}	
+	private static final byte WHITE_SPACE_ENC = -5;
+	private static final byte EQUALS_SIGN_ENC = -1;
 
-	public static void encode(Reader in, Writer out) throws IOException {
-		int c, d, e, k = 0, end = 0;
+	public static final byte[] MASTER_TABLE = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+			'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
+			'4', '5', '6', '7', '8', '9', '+', '/' };
 
-		int u, v, w, x;
+	private static final byte[] DECODE_TABLE = { -9, -9, -9, -9, -9, -9, -9, -9, -9, -5, -5, -9, -9, -5, -9, -9, -9,
+			-9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -5, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, 62,
+			-9, -9, -9, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -9, -9, -9, -1, -9, -9, -9, 0, 1, 2, 3, 4, 5, 6, 7,
+			8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -9, -9, -9, -9, -9, -9, 26, 27, 28,
+			29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -9, -9, -9, -9 };
 
-		while (end == 0) {
-			if ((c = in.read()) == -1) {
-				c = 0;
-				end = 1;
-			}
-
-			if ((d = in.read()) == -1) {
-				d = 0;
-				end += 1;
-			}
-
-			if ((e = in.read()) == -1) {
-				e = 0;
-				end += 1;
-			}  
-			
-			u = MASTER_TABLE[(c & 0xFC) >> 2];
-			v = MASTER_TABLE[(0x03 & c) << 4 | (d & 0xF0) >> 4];
-			w = MASTER_TABLE[(0x0F & d) << 2 | (e & 0xC0) >> 6];
-			x = MASTER_TABLE[e & 0x3F];
-
-			if (k == 76) {
-				k = 0;
-
-				out.write(CRLF);
-			}
-
-			if (end >= 1) {
-				x = PAD;
-
-				if (end == 2) {
-					w = PAD;
-				}
-			}
-
-			if (end < 3) {
-				out.write(u);
-				out.write(v);
-				out.write(w);
-				out.write(x);
-			}
-
-			k += 4;
-		}
+	public static final String encode(byte[] source) {
+		return encode(source, 0, source.length);
 	}
 
-	public static void decode(Reader in, Writer out) throws IOException {
-		int c = 0, d = 0, e = 0, f = 0, i = 0, n = 0;
-
-		do {
-			f = in.read();
-
-			if (f >= 0 && f < 128 && (i = DECODE_TABLE[f]) != -1) {
-				if (n % 4 == 0) {
-					c = i << 2;
-				} else if (n % 4 == 1) {
-					c = c | (i >> 4);
-					d = (i & 0x0f) << 4;
-				} else if (n % 4 == 2) {
-					d = d | (i >> 2);
-					e = (i & 0x03) << 6;
-				} else {
-					e = e | i;
-				}
-
-				n++;
-
-				if (n % 4 == 0) {
-					out.write(c);
-					out.write(d);
-					out.write(e);
-				}
+	public static final String encode(final byte[] src, final int off, final int len) {
+		final int len43 = len * 4 / 3;
+		final byte[] outBuff = new byte[len43 // Main 4:3
+				+ ((len % 3) > 0 ? 4 : 0) // Account for padding
+				+ (len43 / MAX_LINE_LENGTH)]; // New lines
+		int d = 0;
+		int e = 0;
+		final int len2 = len - 2;
+		int lineLength = 0;
+		for (; d < len2; d += 3, e += 4) {
+			encode3to4(src, d + off, 3, outBuff, e);
+			lineLength += 4;
+			if (lineLength == MAX_LINE_LENGTH) {
+				outBuff[e + 4] = NEW_LINE;
+				e++;
+				lineLength = 0;
 			}
-		} while (f != -1);
-
-		if (n % 4 == 3) {
-			out.write(c);
-			out.write(d);
-		} else if (n % 4 == 2) {
-			out.write(c);
 		}
+
+		if (d < len) { // padding needed
+			encode3to4(src, d + off, len - d, outBuff, e);
+			e += 4;
+		}
+
+		return new String(outBuff, 0, e);
 	}
 
-	static {
-		for (int i = 0; i < 128; i++) {
-			DECODE_TABLE[i] = -1;
+	public static final byte[] decode(final String s) throws UnsupportedEncodingException {
+		final byte[] bytes;
+		bytes = s.getBytes("US-ASCII");
+		return decode(bytes, 0, bytes.length);
+	}
+
+	public static byte[] decode(final byte[] src, final int off, final int len) {
+		final int len34 = len * 3 / 4;
+		final byte[] outBuff = new byte[len34]; // Upper limit on size of output
+		int outBuffPosn = 0;
+		final byte[] b4 = new byte[4];
+		int b4Posn = 0;
+		int i;
+		byte sbiCrop, sbiDecode;
+		for (i = off; i < off + len; i++) {
+			sbiCrop = (byte) (src[i] & 0x7F); // Only the low seven bits
+			sbiDecode = DECODE_TABLE[sbiCrop];
+			if (sbiDecode >= WHITE_SPACE_ENC) { // White space, Equals sign or better
+				if (sbiDecode >= EQUALS_SIGN_ENC) {
+					b4[b4Posn++] = sbiCrop;
+					if (b4Posn > 3) {
+						outBuffPosn += decode4to3(b4, 0, outBuff, outBuffPosn);
+						b4Posn = 0;
+						// If that was the equals sign, break out of 'for' loop
+						if (sbiCrop == EQUALS_SIGN)
+							break;
+					} // end if: quartet built
+				} // end if: equals sign or better
+			} else {
+				throw new IllegalArgumentException("Illegal BASE-64 character at #" + i + ": " + src[i] + "(decimal)");
+			}
 		}
 
-		for (int i = 0; i < 64; i++) {
-			DECODE_TABLE[(int) MASTER_TABLE[i]] = i;
+		final byte[] result = new byte[outBuffPosn];
+		System.arraycopy(outBuff, 0, result, 0, outBuffPosn);
+
+		return result;
+	}
+
+	private static final byte[] encode3to4(final byte[] src, final int sOffset, final int numBytes, final byte[] dest,
+			final int dOffset) {
+		final int inBuff = (numBytes > 0 ? ((src[sOffset] << 24) >>> 8) : 0)
+				| (numBytes > 1 ? ((src[sOffset + 1] << 24) >>> 16) : 0)
+				| (numBytes > 2 ? ((src[sOffset + 2] << 24) >>> 24) : 0);
+		switch (numBytes) {
+		case 3:
+			dest[dOffset] = MASTER_TABLE[(inBuff >>> 18)];
+			dest[dOffset + 1] = MASTER_TABLE[(inBuff >>> 12) & 0x3F];
+			dest[dOffset + 2] = MASTER_TABLE[(inBuff >>> 6) & 0x3F];
+			dest[dOffset + 3] = MASTER_TABLE[(inBuff) & 0x3F];
+			break;
+		case 2:
+			dest[dOffset] = MASTER_TABLE[(inBuff >>> 18)];
+			dest[dOffset + 1] = MASTER_TABLE[(inBuff >>> 12) & 0x3F];
+			dest[dOffset + 2] = MASTER_TABLE[(inBuff >>> 6) & 0x3F];
+			dest[dOffset + 3] = EQUALS_SIGN;
+			break;
+		case 1:
+			dest[dOffset] = MASTER_TABLE[(inBuff >>> 18)];
+			dest[dOffset + 1] = MASTER_TABLE[(inBuff >>> 12) & 0x3F];
+			dest[dOffset + 2] = EQUALS_SIGN;
+			dest[dOffset + 3] = EQUALS_SIGN;
+			break;
+		}
+		return dest;
+	}
+
+	private static final int decode4to3(final byte[] src, final int sOffset, final byte[] dest, final int dOffset) {
+		if (src[sOffset + 2] == EQUALS_SIGN) { // Example: Dk==
+			final int outBuff = ((DECODE_TABLE[src[sOffset]] & 0xFF) << 18)
+					| ((DECODE_TABLE[src[sOffset + 1]] & 0xFF) << 12);
+			dest[dOffset] = (byte) (outBuff >>> 16);
+			return 1;
+		}
+
+		if (src[sOffset + 3] == EQUALS_SIGN) { // Example: DkL=
+			final int outBuff = ((DECODE_TABLE[src[sOffset]] & 0xFF) << 18)
+					| ((DECODE_TABLE[src[sOffset + 1]] & 0xFF) << 12) | ((DECODE_TABLE[src[sOffset + 2]] & 0xFF) << 6);
+			dest[dOffset] = (byte) (outBuff >>> 16);
+			dest[dOffset + 1] = (byte) (outBuff >>> 8);
+			return 2;
+		}
+
+		try { // Example: DkLE
+			final int outBuff = ((DECODE_TABLE[src[sOffset]] & 0xFF) << 18)
+					| ((DECODE_TABLE[src[sOffset + 1]] & 0xFF) << 12) | ((DECODE_TABLE[src[sOffset + 2]] & 0xFF) << 6)
+					| ((DECODE_TABLE[src[sOffset + 3]] & 0xFF));
+			dest[dOffset] = (byte) (outBuff >> 16);
+			dest[dOffset + 1] = (byte) (outBuff >> 8);
+			dest[dOffset + 2] = (byte) outBuff;
+			return 3;
+		} catch (Exception x) {
+			return -1;
 		}
 	}
 }
