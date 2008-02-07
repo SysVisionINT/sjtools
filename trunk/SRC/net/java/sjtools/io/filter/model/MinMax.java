a18b03b69c0c22967109e53192cf7b8ad9cf9829
goncalo_pereira <goncalo_pereira@acda1dc6-91a2-4b3b-9694-aea3ce83199f>
2008-02-07 14:47:42 +0000

    Issue number: Obtained from: Submitted by:  Gon�o Pereira Reviewed by:

---
+++ trunk/SRC/net/java/sjtools/io/filter/model/MinMax.java
@@ -0,0 +1,54 @@
+/*
+ * SJTools - SysVision Java Tools
+ * 
+ * Copyright (C) 2008 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.  
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+package net.java.sjtools.io.filter.model;
+
+import java.io.Serializable;
+
+public class MinMax implements Serializable {
+
+	private static final long serialVersionUID = -6082201288975596366L;
+
+	private long min = 0;
+	private long max = 0;
+
+	public MinMax() {}
+
+	public MinMax(long min, long max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	public long getMin() {
+		return min;
+	}
+
+	public void setMin(long min) {
+		this.min = min;
+	}
+
+	public long getMax() {
+		return max;
+	}
+
+	public void setMax(long max) {
+		this.max = max;
+	}
+
+}
