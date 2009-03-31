36108247b618e61ff2a4593bc1249a739f78790b
Joaquim Rocha <jjmrocha@users.noreply.github.com>
2009-03-31 11:40:36 +0000

    *** empty log message ***

--- SRC/net/java/sjtools/messaging/MessageBroker.java
+++ SRC/net/java/sjtools/messaging/MessageBroker.java
@@ -1,18 +1,18 @@
 /*
  * SJTools - SysVision Java Tools
- * 
- * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.  
- * 
+ *
+ * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
@@ -102,9 +102,9 @@ public class MessageBroker {
 		if (registed == null) {
 			registed = new ListenerFeeder(name, listener);
 			listenerMap.put(name, registed);
+		} else {
+			registed.incTopicCount();
 		}
-		
-		registed.incTopicCount();
 
 		listenerLock.releaseLock();
 	}
@@ -129,12 +129,12 @@ public class MessageBroker {
 	public MessageStorage getMessageStorage() {
 		return messageStorage;
 	}
-	
+
 	public ListenerFeeder getListenerFeeder(String listenerName) {
 		listenerLock.getReadLock();
 		ListenerFeeder feeder = (ListenerFeeder) listenerMap.get(listenerName);
 		listenerLock.releaseLock();
-		
+
 		return feeder;
 	}
 }
