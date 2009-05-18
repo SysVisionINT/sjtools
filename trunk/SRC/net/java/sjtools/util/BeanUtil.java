700d11540f75ef3b4895dd0b5f5e534897e7b04a
jrocha <jrocha@acda1dc6-91a2-4b3b-9694-aea3ce83199f>
2009-05-18 14:31:38 +0000

    *** empty log message ***

--- trunk/SRC/net/java/sjtools/util/BeanUtil.java
+++ trunk/SRC/net/java/sjtools/util/BeanUtil.java
@@ -1,18 +1,18 @@
 /*
  * SJTools - SysVision Java Tools
- * 
- * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.  
- * 
+ *
+ * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
@@ -378,10 +378,9 @@ public class BeanUtil {
 		return method;
 	}
 
-	public static Object getPropertyValue(BeanUtil beanUtil, String propertyName) throws Exception {
+	public static BeanUtil getPropertyBean(BeanUtil beanUtil, String propertyName) throws Exception {
 		BeanUtil bu = beanUtil;
 		List list = TextUtil.split(propertyName, ".");
-		String pn = (String) list.get(list.size() - 1);
 
 		Object obj = null;
 
@@ -389,6 +388,15 @@ public class BeanUtil {
 			obj = bu.get((String) list.get(i));
 			bu = new BeanUtil(obj);
 		}
+
+		return bu;
+	}
+
+	public static Object getPropertyValue(BeanUtil beanUtil, String propertyName) throws Exception {
+		List list = TextUtil.split(propertyName, ".");
+		String pn = (String) list.get(list.size() - 1);
+
+		BeanUtil bu = getPropertyBean(beanUtil, propertyName);
 
 		return bu.get(pn);
 	}
