72116d86e84b13f3fa097a79b839bd5c51654f97
Joaquim Rocha <jjmrocha@users.noreply.github.com>
2009-07-02 14:53:41 +0000

    *** empty log message ***

--- SRC/net/java/sjtools/logging/impl/FileWriter.java
+++ SRC/net/java/sjtools/logging/impl/FileWriter.java
@@ -1,18 +1,18 @@
 /*
  * SJTools - SysVision Java Tools
- * 
- * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.  
- * 
+ *
+ * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
@@ -43,7 +43,7 @@ public class FileWriter implements Writer, Config, Serializable {
 	private String charset = null;
 	private SuperDate lastDate = null;
 
-	private PrintWriter printWriter = null;
+	private transient PrintWriter printWriter = null;
 
 	public synchronized void print(String text) {
 		println(text);
