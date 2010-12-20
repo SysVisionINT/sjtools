790b3ad141396ab02bc1ff366ad86a8b44b6d6c9
Joaquim Rocha <jjmrocha@users.noreply.github.com>
2010-12-20 14:48:00 +0000

    *** empty log message ***

--- SRC/net/java/sjtools/db/ds/FastDataSource.java
+++ SRC/net/java/sjtools/db/ds/FastDataSource.java
@@ -1,100 +1,55 @@
 /*
  * SJTools - SysVision Java Tools
- * 
- * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.  
- * 
+ *
+ * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 package net.java.sjtools.db.ds;
 
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
 import net.java.sjtools.db.connection.FastConnection;
-import net.java.sjtools.util.JNDIUtil;
 
-public class FastDataSource implements DataSource {
-	private FastConnection connection = null;
-	private DataSource dataSource = null;
+public class FastDataSource extends DataSourceImpl {
+	private static final long serialVersionUID = -961465900213544680L;
 
-	private FastDataSource(DataSource ds) {
-		dataSource = ds;
-	}
-	
-	public static FastDataSource getInstance(String driver, String url, String user, String password) {
-		return getInstance(new DataSourceImpl(driver, url, user, password));
-	}
+	private static ThreadLocal localCon = new ThreadLocal();
 
-	public static FastDataSource getInstance(String jndiName) throws NamingException {
-		return getInstance((DataSource) JNDIUtil.getJNDIObject(jndiName));
-	}
-
-	public static FastDataSource getInstance(DataSource ds) {
-		return new FastDataSource(ds);
-	}	
-
-	public void release() {
-		if (connection != null) {
-			connection.closeConnection();
-		}
+	public FastDataSource(String driver, String url, String user, String password) {
+		super(driver, url, user, password);
 	}
 
 	public Connection getConnection() throws SQLException {
-			if (connection != null) {
-				return connection;
-			} else {
-				connection = getNewConnection();
-				return connection;
-			}
+		Connection con = (Connection) localCon.get();
+
+		if (con == null) {
+			con = super.getConnection();
+
+			localCon.set(new FastConnection(con));
+		}
+
+		return con;
 	}
 
-	private FastConnection getNewConnection() throws SQLException {
-		Connection connection = dataSource.getConnection();
+	public void release() {
+		FastConnection con = (FastConnection) localCon.get();
 
-		return new FastConnection(connection);
-	}
-
-	public int getLoginTimeout() throws SQLException {
-		return dataSource.getLoginTimeout();
-	}
-
-	public void setLoginTimeout(int time) throws SQLException {
-		dataSource.setLoginTimeout(time);
-	}
-
-	public PrintWriter getLogWriter() throws SQLException {
-		return dataSource.getLogWriter();
-	}
-
-	public void setLogWriter(PrintWriter lw) throws SQLException {
-		dataSource.setLogWriter(lw);
-	}
-
-	public Connection getConnection(String user, String password) throws SQLException {
-		throw new SQLException("Method not supported!");
-	}
-
-	public boolean isWrapperFor(Class arg0) throws SQLException {
-		throw new SQLException("Method not supported!");
-	}
-
-	public Object unwrap(Class arg0) throws SQLException {
-		throw new SQLException("Method not supported!");
+		if (con != null) {
+			con.closeConnection();
+		}
 	}
 }
