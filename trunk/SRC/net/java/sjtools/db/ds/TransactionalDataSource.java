9db4128ba356c2393078e7b676bf4a3ffbeca3c4
jrocha <jrocha@acda1dc6-91a2-4b3b-9694-aea3ce83199f>
2010-12-20 14:48:00 +0000

    *** empty log message ***

--- trunk/SRC/net/java/sjtools/db/ds/TransactionalDataSource.java
+++ trunk/SRC/net/java/sjtools/db/ds/TransactionalDataSource.java
@@ -1,162 +1,129 @@
 /*
  * SJTools - SysVision Java Tools
- * 
- * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.  
- * 
+ *
+ * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 package net.java.sjtools.db.ds;
 
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.naming.NamingException;
-import javax.sql.DataSource;
+import net.java.sjtools.db.connection.FastConnection;
+import net.java.sjtools.db.error.SQLError;
 
-import net.java.sjtools.db.connection.ConnectionListener;
-import net.java.sjtools.db.connection.PoolableConnection;
-import net.java.sjtools.util.JNDIUtil;
+public class TransactionalDataSource extends DataSourceImpl {
+	private static final long serialVersionUID = 4609941383579876104L;
 
-public class TransactionalDataSource implements DataSource, ConnectionListener {
-	private boolean transaction = false;
-	private boolean connectionClose = true;
-	private PoolableConnection connection = null;
-	private DataSource dataSource = null;
+	private static ThreadLocal connection = new ThreadLocal();
+	private static ThreadLocal transaction = new ThreadLocal();
 
-	private TransactionalDataSource(DataSource ds) {
-		dataSource = ds;
+	public TransactionalDataSource(String driver, String url, String user, String password) {
+		super(driver, url, user, password);
 	}
 
-	public static TransactionalDataSource getInstance(String driver, String url, String user, String password) {
-		return getInstance(new DataSourceImpl(driver, url, user, password));
+	public void startTransaction() throws SQLError {
+		transaction.set(Boolean.TRUE);
+
+		Connection con = null;
+
+		try {
+			con = super.getConnection();
+		} catch (SQLException e) {
+			throw new SQLError(e);
+		}
+
+		setAutoCommit(con, false);
+
+		connection.set(new FastConnection(con));
 	}
 
-	public static TransactionalDataSource getInstance(String jndiName) throws NamingException {
-		return getInstance((DataSource) JNDIUtil.getJNDIObject(jndiName));
+	private void setAutoCommit(Connection con, boolean auto) {
+		try {
+			con.setAutoCommit(auto);
+		} catch (SQLException e) {}
 	}
 
-	public static TransactionalDataSource getInstance(DataSource ds) {
-		return new TransactionalDataSource(ds);
-	}
+	public void commit() throws SQLError {
+		Boolean tran = (Boolean) transaction.get();
 
-	public void startTransaction() {
-		transaction = true;
-	}
+		if (tran != null && tran.booleanValue()) {
+			FastConnection con = (FastConnection) connection.get();
 
-	public void commit() throws SQLException {
-		if (transaction) {
-			if (connection != null) {
+			if (con != null) {
 				try {
-					connection.commit();
+					con.commit();
+				} catch (SQLException e) {
+					throw new SQLError(e);
 				} finally {
-					if (connectionClose) {
-						closeConnection();
-					}
+					setAutoCommit(con, true);
+					close(con);
 				}
 			}
 
-			transaction = false;
+			transaction.set(Boolean.FALSE);
+			connection.set(null);
 		}
 	}
 
-	public void rollback() throws SQLException {
-		if (transaction) {
-			if (connection != null) {
+	private void close(FastConnection con) {
+		con.closeConnection();
+	}
+
+	public void rollback() throws SQLError {
+		Boolean tran = (Boolean) transaction.get();
+
+		if (tran != null && tran.booleanValue()) {
+			FastConnection con = (FastConnection) connection.get();
+
+			if (con != null) {
 				try {
-					connection.rollback();
+					con.rollback();
+				} catch (SQLException e) {
+					throw new SQLError(e);
 				} finally {
-					if (connectionClose) {
-						closeConnection();
-					}
+					setAutoCommit(con, true);
+					close(con);
 				}
 			}
 
-			transaction = false;
+			transaction.set(Boolean.FALSE);
+			connection.set(null);
 		}
 	}
 
 	public boolean isInTransaction() {
-		return transaction;
+		Boolean tran = (Boolean) transaction.get();
+
+		if (tran != null && tran.booleanValue()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public Connection getConnection() throws SQLException {
-		if (transaction) {
-			if (connection != null) {
-				return connection;
-			} else {
-				connection = getNewConnection();
-				connectionClose = false;
-				return connection;
-			}
+		if (isInTransaction()) {
+			return (Connection) connection.get();
 		} else {
-			return getNewConnection();
+			return super.getConnection();
 		}
 	}
 
-	private PoolableConnection getNewConnection() throws SQLException {
-		Connection connection = dataSource.getConnection();
-
-		if (transaction) {
-			connection.setAutoCommit(false);
-		}
-
-		return new PoolableConnection(connection, transaction ? this : null);
-	}
-
-	public int getLoginTimeout() throws SQLException {
-		return dataSource.getLoginTimeout();
-	}
-
-	public void setLoginTimeout(int time) throws SQLException {
-		dataSource.setLoginTimeout(time);
-	}
-
-	public PrintWriter getLogWriter() throws SQLException {
-		return dataSource.getLogWriter();
-	}
-
-	public void setLogWriter(PrintWriter lw) throws SQLException {
-		dataSource.setLogWriter(lw);
-	}
-
-	public Connection getConnection(String user, String password) throws SQLException {
-		throw new SQLException("Method not supported!");
-	}
-
-	public void connectionClosed(PoolableConnection con) {
-		connectionClose = true;
-		
-		if (!transaction) {
-			try {
-				closeConnection();
-			} catch (SQLException e) {
-			}
-		}
-	}
-
-	private void closeConnection() throws SQLException {
-		connection.closeConnection();
-		connection = null;
-	}
-
-	public boolean isWrapperFor(Class arg0) throws SQLException {
-		throw new SQLException("Method not supported!");
-	}
-
-	public Object unwrap(Class arg0) throws SQLException {
-		throw new SQLException("Method not supported!");
+	public static TransactionalDataSource getInstance(String driver, String url, String user, String password) {
+		return new TransactionalDataSource(driver, url, user, password);
 	}
 }
