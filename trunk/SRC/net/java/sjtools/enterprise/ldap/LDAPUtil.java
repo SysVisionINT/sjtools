37100d4799c56e2acf3c163ddd917de40474a06e
jrocha <jrocha@acda1dc6-91a2-4b3b-9694-aea3ce83199f>
2011-01-19 11:08:07 +0000

    *** empty log message ***

--- trunk/SRC/net/java/sjtools/enterprise/ldap/LDAPUtil.java
+++ trunk/SRC/net/java/sjtools/enterprise/ldap/LDAPUtil.java
@@ -1,18 +1,18 @@
 /*
  * SJTools - SysVision Java Tools
- * 
- * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.  
- * 
+ *
+ * Copyright (C) 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
@@ -126,6 +126,7 @@ public class LDAPUtil {
 			SearchControls searchCtls = new SearchControls();
 			searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
 			searchCtls.setReturningAttributes(getReturningAttributes(config));
+			searchCtls.setTimeLimit((int) config.getTimeout());
 
 			String user = getUserName(userName);
 			String userFilter = getUserFilter(user);
@@ -140,7 +141,7 @@ public class LDAPUtil {
 					break;
 				}
 			}
-			
+
 			if (data == null) {
 				log.error("User " + user + " not exists");
 				throw new LDAPUserNotExistsError();
